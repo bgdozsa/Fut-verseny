f77f42a18e9c66b6d6277f3fc544e06278fb332a
Balogh Gábor <bgdozsa@gmail.com>
2024-11-03 17:04:42 +0100

    versenyObj

--- Futóverseny/Verseny.cs
+++ Futóverseny/Verseny.cs
@@ -8,5 +8,69 @@ namespace Futóverseny
 {
     class Verseny
     {
+        public Pálya pálya;
+        public SortedSet<int> győztesek;
+        private Versenyző[] versenyzők;
+        private int versenyzőkSzáma;
+        private const int szünet = 150; //várakozás ezredmásodpercben a lépések között
+
+        public void Start()
+        {
+            Console.Title = "Futóverseny";
+            //Beolvassuk a versenyzők számát:
+            Console.Write($"A versenyzők száma (< {Console.LargestWindowHeight - 9}): ");
+            versenyzőkSzáma = Convert.ToInt32(Console.ReadLine());
+            //létrehozzuk a versenyzőobjektumokat (tömbelemek):
+            versenyzők = new Versenyző[versenyzőkSzáma];
+            for (int i = 0; i < versenyzőkSzáma; i++)
+            {
+                versenyzők[i] = new Versenyző();
+                versenyzők[i].Létrehoz(i);
+                Vár(10);    //A várakozásra azért van szükség, mert ha a versenyzők nagyon gyorsan egymás után hozzák létre a Random objektumokat, akkor azonos lesz a sorozatok kezdőértéke. (A kezdőértéket a rendszeridőből határozza meg.)
+
+            }
+            //Létrehozzuk a pályaobjektumot, Kirajzoljuk a pályát:
+            pálya = new Pálya();
+            pálya.Létrehoz(versenyzőkSzáma);
+            //Elindítjuk a versenyt:
+            győztesek = new SortedSet<int>();
+            Futás();
+            //Kiírjuk a győzteseket:
+            Eredmény();
+            Console.Write("(Enter: kilép)");
+            Console.ReadLine();
+        }
+
+        private void Futás()
+        {
+            do
+            {
+                Console.CursorTop = 0;
+                for (int i = 0; i < versenyzőkSzáma; i++)
+                    versenyzők[i].Fut(this);
+                Vár(szünet);
+            } while (győztesek.Count == 0);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Vége a versenynek.");
+        }
+
+        private void Eredmény()
+        {
+            Console.Write("Győzött: ");
+            foreach (var győztes in győztesek)
+            {
+                Console.Write($"{győztes} ");
+            }
+            Console.WriteLine();
+        }
+
+        private void Vár(int időtartam)
+        {
+            System.Threading.Thread.Sleep(időtartam);
+        }
+
+
+
+
     }
 }
